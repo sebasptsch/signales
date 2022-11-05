66dfd7373494746d5438186612855a543b19bd94
Sebastian Pietschner <sebastian.pietschner@gmail.com>
2022-11-05 03:36:33 +0000

    bump version and fix color types

--- src/logger-types.ts
+++ src/logger-types.ts
@@ -1,116 +1,116 @@
-'use strict';
-import figures from 'figures'
-import type { DefaultLoggerTypes } from './types'
+"use strict";
+import figures from "figures";
+import type { DefaultLoggerTypes } from "./types";
 
 const logTypes: DefaultLoggerTypes = {
   error: {
     badge: figures.cross,
-    color: 'red',
-    label: 'error',
-    logLevel: 'error'
+    color: "red",
+    label: "error",
+    logLevel: "error",
   },
   fatal: {
     badge: figures.cross,
-    color: 'red',
-    label: 'fatal',
-    logLevel: 'error'
+    color: "red",
+    label: "fatal",
+    logLevel: "error",
   },
   alert: {
-    badge: figures('⬤'),
-    color: 'red',
-    label: 'alert',
-    logLevel: 'error'
+    badge: "⬤",
+    color: "red",
+    label: "alert",
+    logLevel: "error",
   },
   fav: {
-    badge: figures('❤'),
-    color: 'magenta',
-    label: 'favorite',
-    logLevel: 'info'
+    badge: "❤",
+    color: "magenta",
+    label: "favorite",
+    logLevel: "info",
   },
   info: {
     badge: figures.info,
-    color: 'blue',
-    label: 'info',
-    logLevel: 'info'
+    color: "blue",
+    label: "info",
+    logLevel: "info",
   },
   star: {
     badge: figures.star,
-    color: 'yellow',
-    label: 'star',
-    logLevel: 'info'
+    color: "yellow",
+    label: "star",
+    logLevel: "info",
   },
   success: {
     badge: figures.tick,
-    color: 'green',
-    label: 'success',
-    logLevel: 'info'
+    color: "green",
+    label: "success",
+    logLevel: "info",
   },
   wait: {
     badge: figures.ellipsis,
-    color: 'blue',
-    label: 'waiting',
-    logLevel: 'info'
+    color: "blue",
+    label: "waiting",
+    logLevel: "info",
   },
   warn: {
     badge: figures.warning,
-    color: 'yellow',
-    label: 'warning',
-    logLevel: 'warn'
+    color: "yellow",
+    label: "warning",
+    logLevel: "warn",
   },
   complete: {
     badge: figures.checkboxOn,
-    color: 'cyan',
-    label: 'complete',
-    logLevel: 'info'
+    color: "cyan",
+    label: "complete",
+    logLevel: "info",
   },
   pending: {
     badge: figures.checkboxOff,
-    color: 'magenta',
-    label: 'pending',
-    logLevel: 'info'
+    color: "magenta",
+    label: "pending",
+    logLevel: "info",
   },
   note: {
     badge: figures.bullet,
-    color: 'blue',
-    label: 'note',
-    logLevel: 'info'
+    color: "blue",
+    label: "note",
+    logLevel: "info",
   },
   start: {
     badge: figures.play,
-    color: 'green',
-    label: 'start',
-    logLevel: 'info'
+    color: "green",
+    label: "start",
+    logLevel: "info",
   },
   pause: {
     badge: figures.squareSmallFilled,
-    color: 'yellow',
-    label: 'pause',
-    logLevel: 'info'
+    color: "yellow",
+    label: "pause",
+    logLevel: "info",
   },
   debug: {
     badge: figures.pointerSmall,
-    color: 'gray',
-    label: 'debug',
-    logLevel: 'debug'
+    color: "gray",
+    label: "debug",
+    logLevel: "debug",
   },
   await: {
     badge: figures.ellipsis,
-    color: 'blue',
-    label: 'awaiting',
-    logLevel: 'info'
+    color: "blue",
+    label: "awaiting",
+    logLevel: "info",
   },
   watch: {
     badge: figures.ellipsis,
-    color: 'yellow',
-    label: 'watching',
-    logLevel: 'info'
+    color: "yellow",
+    label: "watching",
+    logLevel: "info",
   },
   log: {
-    badge: '',
-    color: '' as const,
-    label: '',
-    logLevel: 'info'
-  }
-}
+    badge: "",
+    color: "",
+    label: "",
+    logLevel: "info",
+  },
+};
 
-export default logTypes
+export default logTypes;
